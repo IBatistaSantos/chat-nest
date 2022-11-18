91057c519a49c0dda1b80e37971f745d8124e77d
Israel Batista <israelbatiista19@gmail.com>
2022-11-17 22:26:10 -0300

    fix: adding configuration from connection database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,31 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
+import { AppGateway } from './app.gateway';
 import { AppService } from './app.service';
+import { Chat } from './chat/chat.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'db',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'chat',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    ConfigModule.forRoot({
+      isGlobal: true,
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.HOST,
+        port: parseInt(process.env.PORT),
+        username: process.env.USERNAME,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE,
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      }),
+    }),
+    TypeOrmModule.forFeature([Chat]),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppGateway],
 })
 export class AppModule {}
