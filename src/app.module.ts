061c3bd4c63aa625136bcfccc87fee74bc657113
Israel Batista <israelbatiista19@gmail.com>
2022-11-21 20:29:09 -0300

    fix: adding environment variables connection database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppGateway } from './app.gateway';
 import { AppService } from './app.service';
@@ -13,15 +13,16 @@ import { Chat } from './chat/chat.entity';
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: () => ({
+      useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: process.env.HOST,
-        port: parseInt(process.env.PORT),
-        username: process.env.USERNAME,
-        password: process.env.PASSWORD,
-        database: process.env.DATABASE,
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
       }),
+      inject: [ConfigService],
     }),
     TypeOrmModule.forFeature([Chat]),
   ],
--- src/infra/typeorm/config/connection.ts
+++ src/infra/typeorm/config/connection.ts
@@ -4,11 +4,11 @@ import { CreateChat1668726313561 } from '../migrations/1668726313561-CreateChat'
 
 export default new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'chat',
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   entities: [Chat],
   migrations: [CreateChat1668726313561],
 });
